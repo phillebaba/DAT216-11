616f6e1749921e634eae28b404e8aae733294b11
Philip Laine <philip.laine@gmail.com>
2017-02-23 20:57:32 +0100

    Change the root pane and product pane

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,11 +6,13 @@ import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
 import se.chalmers.ait.dat215.project.*;
 
 public class Controller implements ShoppingCartListener {
@@ -28,7 +30,7 @@ public class Controller implements ShoppingCartListener {
     private Button vegetableButton;
 
     @FXML
-    private Button produceButton;
+    private Button meatButton;
 
     @FXML
     private Button breadButton;
@@ -49,19 +51,62 @@ public class Controller implements ShoppingCartListener {
     private TextField searchField;
 
     @FXML
-    private GridPane productPane;
+    private Button searchButton;
 
     @FXML
-    private TableView<?> cartView;
+    private TilePane productPane;
+
+    @FXML
+    private ListView<?> cartView;
 
     @FXML
     void categoryButtonPushed(ActionEvent event) {
         ArrayList<ProductCategory> fruitList = new ArrayList<ProductCategory>();
-        fruitList.add(ProductCategory.FRUIT);
         fruitList.add(ProductCategory.BERRY);
+        fruitList.add(ProductCategory.CITRUS_FRUIT);
+        fruitList.add(ProductCategory.EXOTIC_FRUIT);
+        fruitList.add(ProductCategory.MELONS);
+        fruitList.add(ProductCategory.FRUIT);
+
+        ArrayList<ProductCategory> drinkList = new ArrayList<ProductCategory>();
+        drinkList.add(ProductCategory.HOT_DRINKS);
+        drinkList.add(ProductCategory.COLD_DRINKS);
+
+        ArrayList<ProductCategory> meatList = new ArrayList<ProductCategory>();
+        meatList.add(ProductCategory.FISH);
+        meatList.add(ProductCategory.MEAT);
+
+        ArrayList<ProductCategory> pantryList = new ArrayList<ProductCategory>();
+        pantryList.add(ProductCategory.POD);
+        pantryList.add(ProductCategory.FLOUR_SUGAR_SALT);
+        pantryList.add(ProductCategory.NUTS_AND_SEEDS);
+        pantryList.add(ProductCategory.PASTA);
+        pantryList.add(ProductCategory.POTATO_RICE);
+        pantryList.add(ProductCategory.HERB);
+
+        ArrayList<ProductCategory> dairyList = new ArrayList<ProductCategory>();
+        dairyList.add(ProductCategory.DAIRIES);
+
+        ArrayList<ProductCategory> sweetList = new ArrayList<ProductCategory>();
+        sweetList.add(ProductCategory.SWEET);
+
+        ArrayList<ProductCategory> breadList = new ArrayList<ProductCategory>();
+        breadList.add(ProductCategory.BREAD);
+
+        ArrayList<ProductCategory> vegetableList = new ArrayList<ProductCategory>();
+        vegetableList.add(ProductCategory.VEGETABLE_FRUIT);
+        vegetableList.add(ProductCategory.CABBAGE);
+        vegetableList.add(ProductCategory.ROOT_VEGETABLE);
 
         HashMap<Button, ArrayList<ProductCategory>> hashMap = new HashMap();
         hashMap.put(fruitButton, fruitList);
+        hashMap.put(drinksButton, drinkList);
+        hashMap.put(meatButton, meatList);
+        hashMap.put(pantryButton, pantryList);
+        hashMap.put(dairyButton, dairyList);
+        hashMap.put(sweetButton, sweetList);
+        hashMap.put(breadButton, breadList);
+        hashMap.put(vegetableButton, vegetableList);
 
         ArrayList<ProductCategory> selectedCategories = hashMap.get(event.getSource());
         if (selectedCategories != null) {
@@ -76,26 +121,31 @@ public class Controller implements ShoppingCartListener {
     }
 
     @FXML
+    void searchButtonPushed(ActionEvent event) {
+        //ArrayList<Product> filtered = (ArrayList<Product>) IMatDataHandler.getInstance().getProducts().stream().filter(p -> p.getName().contains(searchField.getCharacters()));
+        updateProducts((ArrayList<Product>) IMatDataHandler.getInstance().getProducts());
+    }
+
+    @FXML
     void initialize() {
         assert fruitButton != null : "fx:id=\"fruitButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert vegetableButton != null : "fx:id=\"vegetableButton\" was not injected: check your FXML file 'sample.fxml'.";
-        assert produceButton != null : "fx:id=\"produceButton\" was not injected: check your FXML file 'sample.fxml'.";
+        assert meatButton != null : "fx:id=\"produceButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert breadButton != null : "fx:id=\"breadButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert pantryButton != null : "fx:id=\"pantryButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert drinksButton != null : "fx:id=\"drinksButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert sweetButton != null : "fx:id=\"sweetButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert dairyButton != null : "fx:id=\"dairyButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert searchField != null : "fx:id=\"searchField\" was not injected: check your FXML file 'sample.fxml'.";
+        assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert productPane != null : "fx:id=\"productPane\" was not injected: check your FXML file 'sample.fxml'.";
         assert cartView != null : "fx:id=\"cartView\" was not injected: check your FXML file 'sample.fxml'.";
 
         IMatDataHandler.getInstance().getShoppingCart().addShoppingCartListener(this);
     }
 
-    void shoppingCartChanged(CartEvent var1) {
-        // Remove all items form cartView
-        // IMatDataHandler.getInstance().getShoppingCart().getItems()
-        // Add new items
+    public void shoppingCartChanged(CartEvent var1) {
+
     }
 
     /**
@@ -105,14 +155,20 @@ public class Controller implements ShoppingCartListener {
      * @param productList List of products to draw
      */
     private void updateProducts(ArrayList<Product> productList) {
-        // One-liner to clear the GridPane
-        //productPane.getChildren().retainAll(productPane.getChildren().get(0));
+        // Empty all of the existing items
+        productPane.getChildren().clear();
 
         // Draw the new product list
         for (Product product: productList) {
-            System.out.println(product.getName());
-            //Button button = new Button();
-            //productPane.add(button, 0, 0);
+            Button button = new Button();
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    IMatDataHandler.getInstance().getShoppingCart().addProduct(product);
+                }
+            });
+            button.setText(product.getName());
+            productPane.getChildren().add(button);
         }
     }
 
