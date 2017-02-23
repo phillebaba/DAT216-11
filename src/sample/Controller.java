e628a71af5e8ae0eec0fced31ac1d0e4dfba483a
Philip Laine <philip.laine@gmail.com>
2017-02-23 15:59:03 +0100

    Add button event mapping to category

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,11 @@
 package sample;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
@@ -15,7 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import se.chalmers.ait.dat215.project.*;
 
-public class Controller {
+public class Controller implements ShoppingCartListener {
 
     @FXML
     private ResourceBundle resources;
@@ -57,6 +55,27 @@ public class Controller {
     private TableView<?> cartView;
 
     @FXML
+    void categoryButtonPushed(ActionEvent event) {
+        ArrayList<ProductCategory> fruitList = new ArrayList<ProductCategory>();
+        fruitList.add(ProductCategory.FRUIT);
+        fruitList.add(ProductCategory.BERRY);
+
+        HashMap<Button, ArrayList<ProductCategory>> hashMap = new HashMap();
+        hashMap.put(fruitButton, fruitList);
+
+        ArrayList<ProductCategory> selectedCategories = hashMap.get(event.getSource());
+        if (selectedCategories != null) {
+            ArrayList<Product> products = new ArrayList<Product>();
+
+            for (ProductCategory productCategory : selectedCategories) {
+                products.addAll(IMatDataHandler.getInstance().getProducts(productCategory));
+            }
+
+            updateProducts(products);
+        }
+    }
+
+    @FXML
     void initialize() {
         assert fruitButton != null : "fx:id=\"fruitButton\" was not injected: check your FXML file 'sample.fxml'.";
         assert vegetableButton != null : "fx:id=\"vegetableButton\" was not injected: check your FXML file 'sample.fxml'.";
@@ -70,26 +89,31 @@ public class Controller {
         assert productPane != null : "fx:id=\"productPane\" was not injected: check your FXML file 'sample.fxml'.";
         assert cartView != null : "fx:id=\"cartView\" was not injected: check your FXML file 'sample.fxml'.";
 
+        IMatDataHandler.getInstance().getShoppingCart().addShoppingCartListener(this);
     }
 
+    void shoppingCartChanged(CartEvent var1) {
+        // Remove all items form cartView
+        // IMatDataHandler.getInstance().getShoppingCart().getItems()
+        // Add new items
+    }
 
-    @FXML
-    void categoryButtonPushed(ActionEvent event) {
-        ArrayList<Product> products = new ArrayList<>(IMatDataHandler.getInstance().getProducts(ProductCategory.FRUIT));
+    /**
+     * Will clear all of the products in the productPane and then draw
+     * out the new views for each of the products in the productList.
+     *
+     * @param productList List of products to draw
+     */
+    private void updateProducts(ArrayList<Product> productList) {
+        // One-liner to clear the GridPane
+        //productPane.getChildren().retainAll(productPane.getChildren().get(0));
 
-        ArrayList<ProductCategory> categoryList = new ArrayList<ProductCategory>();
-        categoryList.add(ProductCategory.FRUIT);
-        categoryList.add(ProductCategory.BERRY);
-
-        HashMap<Object, ProductCategory> hashMap = new HashMap<Object, ProductCategory>();
-        hashMap.put(fruitButton, ProductCategory.FRUIT);
-
-        ProductCategory selectedCategory = hashMap.get(event.getSource());
-        if (selectedCategory != null) {
-
+        // Draw the new product list
+        for (Product product: productList) {
+            System.out.println(product.getName());
+            //Button button = new Button();
+            //productPane.add(button, 0, 0);
         }
     }
-
-    
 
 }
