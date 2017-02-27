38b3d2f20749006878737f023f652bef4382b3f8
Philip Laine <philip.laine@gmail.com>
2017-02-27 19:12:26 +0100

    Initial working prototype of product view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -135,7 +137,6 @@ public class Controller implements ShoppingCartListener {
         Predicate<Product> predicate = p -> p.getName().toLowerCase().contains(searchField.getText().toLowerCase());
         List<Product> filter = products.stream().filter(predicate).collect(Collectors.toList());
         updateProducts((ArrayList<Product>) filter);
-
     }
 
     @FXML
@@ -181,15 +182,10 @@ public class Controller implements ShoppingCartListener {
 
         // Draw the new product list
         for (Product product : productList) {
-            Button button = new Button();
-            button.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    IMatDataHandler.getInstance().getShoppingCart().addProduct(product);
-                }
-            });
-            button.setText(product.getName());
-            productPane.getChildren().add(button);
+            ProductViewCell cell = new ProductViewCell();
+            cell.initialize();
+            cell.updateItem(product);
+            productPane.getChildren().add(cell);
         }
     }
 }
