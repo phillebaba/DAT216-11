c96222fe0a1eeacbb5b38006f988cba70cb6c107
Philip Laine <philip.laine@gmail.com>
2017-03-08 11:51:09 +0100

    Att call to shut down so that data is saved

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,29 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception{
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                IMatDataHandler.getInstance().shutDown();
+            }
+        });
+
         Parent root = FXMLLoader.load(getClass().getResource("resources/sample.fxml"));
-        primaryStage.setTitle("iMat");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        stage.setTitle("iMat");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 
